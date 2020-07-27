4865b710120fa0ab8fdf945aedce1432dd3e5f67
Shoaib Ahmed <64016075+ShoaibAhmed230@users.noreply.github.com>
2020-07-27 11:01:24 -0700

    starting

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let night=document.querySelector("body");
+// let day=document.querySelector("button");
+
+function chngtheme(){
+    night.style.background="#222";
+    // day.innerHTML='img src="images/icons8-sun-40.png"';
+
+    alert(hello);
+}
